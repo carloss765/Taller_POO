850a46bdc63008a6004e28a8c4f5ebc1d28b9e29
Brahian Mesa <brahian.mesa@ucp.edu.co>
2025-03-10 12:14:53 -0500

    Taller_OPP-14

--- 14-Inverso/inverso/src/App.java
+++ 14-Inverso/inverso/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Inverso inverso = new Inverso();
+        inverso.pedirNumeros();
+        inverso.mostrarNumerosInverso();
     }
 }
