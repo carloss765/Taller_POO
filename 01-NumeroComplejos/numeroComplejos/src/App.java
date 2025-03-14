6898856d08a115a9d01d4faa439430d7358b144d
carloss765 <carlosandresmartinezortiz632@gmail.com>
2025-03-13 20:19:57 -0500

    Documentado

--- 01-NumeroComplejos/numeroComplejos/src/App.java
+++ 01-NumeroComplejos/numeroComplejos/src/App.java
@@ -1,33 +1,61 @@
+// Importamos la clase Scanner para poder leer entrada del usuario desde la consola
 import java.util.Scanner;
 
+/**
+ * Clase principal que contiene el punto de entrada del programa para operaciones con números complejos
+ */
 public class App {
+    /**
+     * Método principal que inicia la ejecución del programa
+     * @param args Argumentos de línea de comandos (no utilizados en este programa)
+     * @throws Exception Posibles excepciones durante la ejecución
+     */
     public static void main(String[] args) throws Exception {
+        // Creamos un objeto Scanner para leer la entrada del usuario
         Scanner sc = new Scanner(System.in);
 
+        // Inicializamos dos números complejos con valores 0 + 0i
         Complejo c1 = new Complejo(0, 0);
         Complejo c2 = new Complejo(0, 0);
 
+        // Solicitamos y leemos la parte real del primer número complejo
         System.out.println("Introduce la parte real del primer número complejo:");
         c1.setReal(sc.nextDouble());
 
+        // Solicitamos y leemos la parte imaginaria del primer número complejo
         System.out.println("Introduce la parte imaginaria del primer número complejo:");
         c1.setImaginario(sc.nextDouble());
 
+        // Solicitamos y leemos la parte real del segundo número complejo
         System.out.println("Introduce la parte real del segundo número complejo:");
         c2.setReal(sc.nextDouble());
 
+        // Solicitamos y leemos la parte imaginaria del segundo número complejo
         System.out.println("Introduce la parte imaginaria del segundo número complejo:");
         c2.setImaginario(sc.nextDouble());
 
+        // Cerramos el Scanner para liberar recursos
         sc.close();
 
+        // Mostramos el encabezado de los resultados
         System.out.println("Resultados de las operaciones:");
-        System.out.println("Suma: " + c1.suma(c1));
-        System.out.println("Resta: " + c1.resta(c2));
-        System.out.println("Multiplicación: " + c1.multiplicacion(c1));
-        System.out.println("División: " + c1.division(c2));
-            c1.acumulacion(c2);
-        System.out.println("Acumulador: " + c1);
 
+        // Realizamos y mostramos la suma de los números complejos
+        System.out.println("Suma: " + c1.suma(c1));
+
+        // Realizamos y mostramos la resta de los números complejos
+        System.out.println("Resta: " + c1.resta(c2));
+
+        // Realizamos y mostramos la multiplicación de los números complejos
+        System.out.println("Multiplicación: " + c1.multiplicacion(c1));
+
+        // Realizamos y mostramos la división de los números complejos
+        System.out.println("División: " + c1.division(c2));
+
+        // Acumulamos el segundo número complejo en el primero
+        c1.acumulacion(c2);
+
+        // Mostramos el resultado de la acumulación
+        System.out.println("Acumulador: " + c1);
     }
 }
--- 01-NumeroComplejos/numeroComplejos/src/Complejo.java
+++ 01-NumeroComplejos/numeroComplejos/src/Complejo.java
@@ -1,53 +1,80 @@
+
+// Clase que representa un número complejo con parte real e imaginaria
 public class Complejo {
+    // Almacena la parte real del número complejo
     private double real;
+    // Almacena la parte imaginaria del número complejo
     private double imaginario;
 
+    // Constructor que inicializa un número complejo con sus partes real e imaginaria
     public Complejo(double real, double imaginario) {
+        // Asigna el valor real recibido al atributo real
         this.real = real;
+        // Asigna el valor imaginario recibido al atributo imaginario
         this.imaginario = imaginario;
     }
 
+    // Método getter que devuelve la parte real del número complejo
     public double getReal() {
         return real;
     }
 
+    // Método getter que devuelve la parte imaginaria del número complejo
     public double getImaginario() {
         return imaginario;
     }
 
+    // Método setter que establece un nuevo valor para la parte real
     public void setReal(double real) {
         this.real = real;
     }
 
+    // Método setter que establece un nuevo valor para la parte imaginaria
     public void setImaginario(double imaginario) {
         this.imaginario = imaginario;
     }
 
+    // Método que suma dos números complejos
     public Complejo suma(Complejo c) {
+        // Retorna un nuevo número complejo con la suma de las partes reales e imaginarias
         return new Complejo(real + c.getReal(), imaginario + c.getImaginario());
     }
 
+    // Método que resta dos números complejos
     public Complejo resta(Complejo c) {
+        // Retorna un nuevo número complejo con la resta de las partes reales e imaginarias
         return new Complejo(real - c.getReal(), imaginario - c.getImaginario());
     }
 
+    // Método que multiplica dos números complejos
     public Complejo multiplicacion(Complejo c) {
+        // Retorna un nuevo número complejo aplicando la fórmula de multiplicación de complejos:
+        // (a + bi)(c + di) = (ac - bd) + (ad + bc)i
         return new Complejo(real * c.getReal() - imaginario * c.getImaginario(),
                 real * c.getImaginario() + imaginario * c.getReal());
     }
 
+    // Método que divide dos números complejos
     public Complejo division(Complejo c) {
+        // Calcula el denominador común (c² + d²)
         double denominador = c.getReal() * c.getReal() + c.getImaginario() * c.getImaginario();
+        // Retorna un nuevo número complejo aplicando la fórmula de división de complejos:
+        // (a + bi)/(c + di) = ((ac + bd)/(c² + d²)) + ((bc - ad)/(c² + d²))i
         return new Complejo((real * c.getReal() + imaginario * c.getImaginario()) / denominador,
                 (imaginario * c.getReal() - real * c.getImaginario()) / denominador);
     }
 
+    // Método que acumula (suma) otro número complejo al actual
     public void acumulacion(Complejo c) {
+        // Suma la parte real del otro número complejo a la parte real actual
         this.real += c.getReal();
+        // Suma la parte imaginaria del otro número complejo a la parte imaginaria actual
         this.imaginario += c.getImaginario();
     }
 
+    // Método que convierte el número complejo a su representación en String
     public String toString() {
+        // Retorna el número complejo en formato "a + bi"
         return real + " + " + imaginario + "i";
     }
 }
